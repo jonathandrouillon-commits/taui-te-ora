4220317c0bb1cddd811adad41d8a326a42c668f7
Jonathan Drouillon <jonathan.drouillon@gmail.com>
2026-07-08 11:45:03 -1000

    Remove wrong dashboard page file

--- app/components/dashboard/page.tsx
+++
@@ -1,4 +0,0 @@
-import DashboardLayout from "../../components/dashboard/DashboardLayout";
-import StatCard from "../../components/dashboard/StatCard";
-import RecentRequests from "../../components/dashboard/RecentRequests";
-import RecentNotifications from "../../components/dashboard/RecentNotifications";
