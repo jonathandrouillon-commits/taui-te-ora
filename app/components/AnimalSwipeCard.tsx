495c4c96f456b5fd98fce1bb5bfa0fd5ad0f3f75
Jonathan Drouillon <jonathan.drouillon@gmail.com>
2026-09-07 17:33:23 -1000

    Enlarge new departure badge

--- app/components/AnimalSwipeCard.tsx
+++ app/components/AnimalSwipeCard.tsx
@@ -1010,6 +1010,30 @@ export default function AnimalSwipeCard({
           </div>
         )}
 
+        {showNewBadge && (
+          <img
+            src="/badges/nouveau-depart.png"
+            alt="Nouveau départ"
+            draggable={false}
+            className="
+              pointer-events-none
+              absolute
+              right-[62px]
+              top-[76px]
+              z-[65]
+              h-[92px]
+              w-[92px]
+              object-contain
+              drop-shadow-lg
+              sm:right-[68px]
+              sm:top-[88px]
+              sm:h-[104px]
+              sm:w-[104px]
+              scale-[1.25]
+            "
+          />
+        )}
+
         {/* ACTIONS EN HAUT A DROITE : LIKES / CHOISIR / PARTAGER */}
 
         <div
@@ -1455,29 +1479,6 @@ export default function AnimalSwipeCard({
                 </button>
               )}
 
-              <div className="relative flex shrink-0 items-end">
-                {showNewBadge && (
-                  <img
-                    src="/badges/nouveau-depart.png"
-                    alt="Nouveau départ"
-                    draggable={false}
-                    className="
-                      pointer-events-none
-                      absolute
-                      bottom-[42px]
-                      right-[-8px]
-                      z-[65]
-                      h-[82px]
-                      w-[82px]
-                      object-contain
-                      drop-shadow-lg
-                      sm:bottom-[46px]
-                      sm:h-[92px]
-                      sm:w-[92px]
-                    "
-                  />
-                )}
-
               <button
                 type="button"
                 onClick={(event) => {
@@ -1511,7 +1512,6 @@ export default function AnimalSwipeCard({
               >
                 i
               </button>
-              </div>
             </div>
           </div>
 
